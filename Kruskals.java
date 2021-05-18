e4dbd0e9e62955f034d31bf39ded93a349a22b88
Evan <66287338+EvanR-Dev@users.noreply.github.com>
2021-05-17 23:21:29 -0500

    Create Kruskals.java
    Main code

---
+++ Kruskals.java
@@ -0,0 +1,12 @@
+public class Kruskals {
+  public static voud main(String[] args) {
+    // contain list for edges and map for vertices
+		ArrayList<Edge> edges = new ArrayList<>();
+		HashMap<String, Integer> vertices = new HashMap<>();
+		
+		// construct Kruskals obj by reading file and storing edges and vertices
+    Kruskals k = new Kruskals(edges, vertices);
+    
+    
+  }
+}
